c91a774e38426229348fee4149908ffc77e340b6
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-13 11:55:23 -0700

    second code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,8 @@
  * return - exit point
  *
  */
-int main(void) {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+int main(void) 
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
