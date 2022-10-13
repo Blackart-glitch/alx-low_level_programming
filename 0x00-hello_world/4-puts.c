73825926e5b8adc9276e0e9db57d07252697008f
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-13 10:33:29 -0700

    first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
 	/*string initialization*/
-	char text[]="Programming is like building a multilingual puzzle";
-	puts('"'.text);
+	char text[]="\"Programming is like building a multilingual puzzle";
+	puts(text);
 	return 0;
 }
