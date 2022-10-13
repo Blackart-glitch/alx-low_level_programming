7129474f07447f2b8c9be6217b191cef162a22aa
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-13 12:23:58 -0700

    third code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <unistd.h>
+#include<stdio.h>
+
 /**
- *main-program entry point.
+ * main - Entry point
  *
- *Return: 1 on success.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
 	return (1);
 }
