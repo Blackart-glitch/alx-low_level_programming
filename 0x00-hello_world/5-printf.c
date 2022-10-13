8d563e2a4fa9779fab894ced12c0051c7d142a92
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-13 11:43:12 -0700

    second code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - the ebtry point 
  *
  * return - exit point
  *
  */
-int main (void) {
+int main(void) {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
