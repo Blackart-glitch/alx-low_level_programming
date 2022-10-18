85a1ac93594a8ca5e70e09c3dfee2c451e86fd36
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-18 13:45:54 -0700

    second task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,17 @@
 /**
  * main - Entry point
  *
- * Description: This function print_alphabet() prints all of the alphabets in lower case
+ * Description: prints all of the alphabets in lower case
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char c;
-	for(c = 'a'; c <= 'z'; c++)
+
+	for (c = 'a' ; c <= 'z' ; c++)
 	{
 		_putchar(c);
 	}
+
 	_putchar('\n');
 }
