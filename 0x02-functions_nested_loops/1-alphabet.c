af9d44ec0fdc293d46234c58cd687ce267820025
Daniel Boyé <boyelanrewaju@gmail.com>
2022-10-18 20:42:34 +0100

    Update 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: This function print_alphabet() prints all of the alphabets in lower case
+ * Return: Always 0 (Success)
+ */
+    void print_alphabet(void)
+    {
+    char c ;
+      for(c = 'a'; c <= 'z'; c++)
+      {
+          putchar(c);
+          putchar('\n');
+      }
+    }
+    int main (void)
+    {
+    return (0);
+    }
