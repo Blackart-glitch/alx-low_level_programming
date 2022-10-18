885275718859dfc4b470d37dbef924882e5403e1
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-18 11:53:17 -0700

    first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This prints the character   => _putchar   <=
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+}
