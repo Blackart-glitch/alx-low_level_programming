20824bdc37675f590cdf2476910043e8f10ad8c5
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-18 14:03:41 -0700

    second task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - prints all of the alphabets in lower case.
  *
- * print_alphabet- prints all of the alphabets in lower case.
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
