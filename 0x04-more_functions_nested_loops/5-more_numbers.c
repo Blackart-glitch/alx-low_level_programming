b2adb5c31aa599521a80d09b09bc8782782140db
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-20 10:04:33 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 01234567891011121314\n x10
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, c;
+
+	for (c = 0; c <= 10; c++}
+		{
+		for (i = 0; i <= 14; i++}
+			{
+			_putchar(i);
+			}
+		_putchar('\n');	
+		}
+}
