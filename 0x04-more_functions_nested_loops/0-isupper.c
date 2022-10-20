368d3097aefa096c3e3b008d553a183584f35865
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-20 06:33:18 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isupper - finds uppercase
+ *@c:  varable to test
+ *
+ * Return: 1 (success) 0 (failed)
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (a == i)
+		{
+		return (1);
+		}
+		else
+		{
+		return (0);
+		}
+	}
+}
