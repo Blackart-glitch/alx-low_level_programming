281fbd83b722f523289b0ce460826e42a01625d6
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-20 09:46:37 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#incluse "main.h"
+
+/**
+ * print_numbers - print all single digits
+ *
+ * Return: 0 (success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
