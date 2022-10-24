0a9ef63e44e0b26c73187caeb8eaa2bd68af69d7
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-24 14:04:26 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - resets value to 98
+ * @a: the pointer 1
+ * @b: the pointer 2
+ *
+ * Return: returns result
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*a = *c;
+	*b = *a;
+	*c = *b;
+}
+
