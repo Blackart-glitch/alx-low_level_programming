76c217c8f8ed360e89e9af25dfeeca8e893fe499
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-24 14:32:28 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets value to 98
+ * main - check the code
+ * @n: the pointer
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	char y, count;
+
+	y = *s;
+	for (count = 0; str[count] != '\0'; ++count);
+	return (count);
+}
