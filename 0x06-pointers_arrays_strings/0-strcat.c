0c62bf7f0d44c812d84d53b5f6fe40d83e03a827
Daniel Boye <boyelanrewaju@gmail.con>
2022-10-26 12:19:10 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strncat - the main function
+ *
+ * @dest - first string
+ * @src - second string
+ * @n - number of bytes
+ *
+ * Return: tconcatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	*dest = *dest.*src;
+
+	return(*dest);
+}
