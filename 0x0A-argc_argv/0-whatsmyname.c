ef9f92e8e373f4614bf7582f1e902332a76806fe
Daniel Boye <boyelanrewaju@gmail.con>
2022-11-04 10:27:27 -0700

    test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - displays the program name
+ * @argc: the argument counter
+ * @argv[]: the argument vector
+ *
+ * Return: returns 0 on success
+ */
+main(int argc, int argv[8]){
+	_putchar(argv[0]);
+
+	return (0);
+}
