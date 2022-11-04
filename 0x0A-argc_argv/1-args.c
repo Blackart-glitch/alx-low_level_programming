8bc66ec84e9b539c81d331df4242b98ed8b86ef4
Daniel Boye <boyelanrewaju@gmail.con>
2022-11-04 10:55:14 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument counter 
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int c;
+
+	c = argc - 1;
+	printf(%i\n, c);
+
+	return (0);
+}
